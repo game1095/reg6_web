21683f39a781c52983652760bd4873ed512283dc
game1095 <taweesak.in@gmail.com>
2024-06-25 11:46:32 +0700

    setup bootstrap

--- db/migrate/20240619041810_create_departments.rb
+++ db/migrate/20240619041810_create_departments.rb
@@ -2,7 +2,7 @@ class CreateDepartments < ActiveRecord::Migration[5.2]
   def change
     create_table :departments do |t|
       t.string :name
-
+      t.string :aka
       t.timestamps
     end
   end
